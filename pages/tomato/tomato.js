eb72c2f8ea309b52b2a43af2e5c68e014f025292
YjjTT <yjjtt@tempot.com>
2019-06-29 17:56:20 +0800

    番茄倒计时

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -1,22 +1,69 @@
 // pages/tomato/tomato.js
 Page({
 
+  timer: null,
   /**
    * 页面的初始数据
    */
   data: {
-    time: 1500,
-    formatTime: ""
+    defaultSecond: 5,
+    formatTime: "",
+    timerStatus: "stop",
+    confirmVisable: false,
+    againButtonVisable: false
   },
 
   
   onShow: function () {
+    this.beginCountdown()
+  },
+  beginCountdown(){
+    this.setData({ timerStatus: "start" })
     this.formatTime()
+    this.timer = setInterval(() => {
+      this.data.defaultSecond--
+      this.formatTime()
+      if (this.data.defaultSecond === 0){
+        this.setData({ againButtonVisable: true })
+        this.clearTimer()
+        return
+      }
+    }, 1000)
+  },
+  clearTimer(){
+    clearInterval(this.timer)
+    this.timer = null
+    this.setData({ timerStatus: "stop"})
   },
 
+  showConfirm(){
+    this.clearTimer()
+    this.setData({ confirmVisable: true })
+  },
+  hideConfirm(){
+    this.beginCountdown()
+    this.setData({ confirmVisable: false })
+  },
+  confirmGiveup(event){
+    let content = event.detail
+    wx.navigateBack({
+      to: -1
+    })
+  },
+  againTime(){
+    this.setData({ defaultSecond: 5, againButtonVisable: false })
+    this.beginCountdown()
+  },
+  confirmFinish(event){
+    let content = event.detail
+  },
+  confirmCancel(){
+    this.setData({ defaultSecond: 5, againButtonVisable: false })
+    this.formatTime()
+  },
   formatTime(){
-    let m = Math.floor(this.data.time/60)
-    let s = Math.floor(this.data.time%60)
+    let m = Math.floor(this.data.defaultSecond/60)
+    let s = Math.floor(this.data.defaultSecond%60)
     if(s === 0){
       s = "00"
     }
