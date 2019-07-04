fe57cfa3e3d198ce16cdc774c69b29c23a781d15
YjjTT <yjjtt@tempot.com>
2019-07-04 18:35:22 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',
-    app_id: "wxe80ea825e2cd4ef1",
-    app_secret: "155c8fe004980268db9cb737487fe386",
-    t_app_id: "ChypEEVc3f3LSDoCJgcJQy3c",
-    t_app_secret: "CWt3N4GL4dkgcQ2J4SgYowEW"
-  }
-})
